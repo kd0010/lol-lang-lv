6aaea6cd29fecce74d643d3ab481c796c1983654
David Kander <kanders.davids@gmail.com>
2023-10-26 22:51:00 +0300

    dev

--- src/dev/nodeApp.ts
+++ src/dev/nodeApp.ts
@@ -1,14 +1,20 @@
 import * as express from 'express'
 import * as cors from 'cors'
+import { config } from 'dotenv'
+import { Sheets } from '../modules/lol-personal-db/src/objects/Sheets'
+
+config({path: '.env.local'})
+const SPREADHSEET_ID = process.env['SPREADSHEET_ID']
+const spreadsheetIdErrMsg = 'put SPREADSHEET_ID in .env.local'
 
 const port = 8080
 const app = express()
 app.use(cors())
 
-app.get('/', async (req, res) => {
-  const out: any[] = [
-  ]
-  res.send(out)
+app.get('/sheets', async (req, res) => {
+  if (SPREADHSEET_ID == null) throw spreadsheetIdErrMsg
+
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
