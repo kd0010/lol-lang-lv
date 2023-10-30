475c65d545aca1d8b0182a21cbc2f31acc1bcef1
David Kander <kanders.davids@gmail.com>
2023-10-30 14:41:59 +0200

    entry id stuff (ughhh.. so freaking mind numbing

--- src/functions/getEntryIdsByChampion.ts
+++ src/functions/getEntryIdsByChampion.ts
@@ -1,30 +1,92 @@
 import { isTftEntryIdPart } from '../constants/TftEntryIdParts'
 import { entries } from '../constants/entries'
-import { ChampionId } from 'lol-constants'
+import { ChampionId, getChampionNameById } from 'lol-constants'
+import { Champion } from 'lol-constants/assets'
 
-// TEMP all those generate_tip, stat_stone,
-// skin_whatever have to be meta-analyzed and then through that
-// the entry IDs have to be gotten, otherwise entryId.includes('vi')
-// returns also viktor, some_kind_of_behaVIor for kogmaw etc.
 export function getEntryIdsByChampion(
   championId: ChampionId,
   {
     ignoreTftIds=false,
+    includeGeneratedTipIds=false,
   }: GetEntryIdsByChampionOptions={},
-): string[] {
-  const entryIds: string[] = []
+) {
+  const entryIds: ChampionEntryIds = {
+    name: [],
+    title: [],
+    p: [],
+    q: [],
+    w: [],
+    e: [],
+    r: [],
+    otherAbilityIds: [],
+    skins: [],
+    eternals: [],
+    tft: [],
+    misc: [],
+  }
   const lowercaseChampionId = championId.toLowerCase()
+  const abilities = {
+    p1: `${lowercaseChampionId}passive`,
+    p2: `${lowercaseChampionId}p`,
+    q: `${lowercaseChampionId}q`,
+    w: `${lowercaseChampionId}w`,
+    e: `${lowercaseChampionId}e`,
+    r: `${lowercaseChampionId}r`,
+  }
 
   for (let entryId in entries) {
+    if (!entryId.includes(lowercaseChampionId)) continue
+    if (!includeGeneratedTipIds && entryId.includes('generatedtip_')) continue
+
+    const entryText: string = entries[entryId]
     entryId = entryId.toLowerCase()
-    if (entryId.includes(lowercaseChampionId)) {
-      // ignore TFT IDs stuff
-      if (ignoreTftIds) {
-        const entryIdsParts = entryId.split('_')
-        if (entryIdsParts.some(isTftEntryIdPart)) continue
+    const entryIdsParts = entryId.split('_')
+    const champ = Champion.data[championId]
+    Champion.data['Aatrox'].title
+
+    // tft stuff
+    if (entryIdsParts.some(isTftEntryIdPart)) {
+      if (ignoreTftIds) continue
+      
+      if (
+        (
+          entryId.includes('tft') ||
+          entryId.includes('companion') ||
+          entryId.includes('chibi')
+        ) && (
+          !entryId.includes('yuumipcompanionshipbuff') // exception
+        )
+      ) {
+        entryIds.tft.push(entryId)
       }
 
-      entryIds.push(entryId)
+      continue // if tft done, then entry done
+    }
+
+    if (entryText == getChampionNameById(championId)) {
+      entryIds.name.push(entryId)
+    } else if (entryText == champ.title) {
+      entryIds.title.push(entryId)
+    } else if (entryIdsParts.some(part => part == 'spell')) {
+      if (entryId.includes(abilities.p1) || entryId.includes(abilities.p2)) {
+        entryIds.p.push(entryId)
+      } else if (entryId.includes(abilities.q)) {
+        entryIds.q.push(entryId)
+      } else if (entryId.includes(abilities.w)) {
+        entryIds.w.push(entryId)
+      } else if (entryId.includes(abilities.e)) {
+        entryIds.e.push(entryId)
+      } else if (entryId.includes(abilities.r)) {
+        entryIds.r.push(entryId)
+      } else {
+        entryIds.otherAbilityIds.push(entryId)
+      }
+    } else if (entryId.includes('skin')) {
+      entryIds.skins.push(entryId)
+    } else if (entryId.includes('stat_stone')) {
+      entryIds.eternals.push(entryId)
+    } else {
+      entryIds.misc.push(entryId)
     }
   }
 
@@ -33,4 +95,20 @@ export function getEntryIdsByChampion(
 
 export interface GetEntryIdsByChampionOptions {
   ignoreTftIds?: boolean
+  includeGeneratedTipIds?: boolean
+}
+
+export interface ChampionEntryIds {
+  name: string[]
+  title: string[]
+  p: string[]
+  q: string[]
+  w: string[]
+  e: string[]
+  r: string[]
+  otherAbilityIds: string[]
+  skins: string[]
+  eternals: string[]
+  tft: string[]
+  misc: string[]
 }
--- src/functions/meta-analysis/getEntryIdsForChampions.ts
+++ src/functions/meta-analysis/getEntryIdsForChampions.ts
@@ -1,10 +1,14 @@
-import { GetEntryIdsByChampionOptions, getEntryIdsByChampion } from '../getEntryIdsByChampion'
+import {
+  ChampionEntryIds,
+  GetEntryIdsByChampionOptions,
+  getEntryIdsByChampion,
+} from '../getEntryIdsByChampion'
 import { ChampionIds } from 'lol-constants'
 
 export function getEntryIdsForChampions(
   options?: GetEntryIdsByChampionOptions,
-): {[championId: string]: string[]} {
-  const product: {[championId: string]: string[]} = {}
+) {
+  const product: {[champId: string]: ChampionEntryIds} = {}
   const champIds = Object.values(ChampionIds)
 
   for (const champId of champIds) {
